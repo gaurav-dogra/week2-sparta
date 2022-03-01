161b45dbe39d3751a7f60bad6466588024c0c13b
Gaurav Dogra <gauru.dogra@gmail.com>
2022-03-01 23:30:38 +0000

    updated Factorial return value from int to long, used tolerance value for flaoting number comparisons

--- Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
+++ Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -5,9 +5,9 @@ namespace DataTypes_Lib
     public static class Methods
     {
         // write a method to return the product of all numbers from 1 to n inclusive
-        public static int Factorial(int n)
+        public static long Factorial(int n)
         {
-            int factorial = 1;
+            long factorial = 1;
             for (int i = n; i >= 1; i--)
             {
                 factorial *= i;
@@ -17,7 +17,7 @@ namespace DataTypes_Lib
 
         public static float Mult(float num1, float num2)
         {
-            return (float) Math.Round(num1 * num2, 5);
+            return num1 * num2;
         }
     }
 }
