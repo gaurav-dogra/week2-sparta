57922b21a2e596c4eb194b69cf7d85322847bc65
Gaurav Dogra <gauru.dogra@gmail.com>
2022-03-02 19:17:10 +0000

    solved ArrayExercises and DateTimeEnumExercises

--- MoreDataTypes_Labs/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ MoreDataTypes_Labs/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -8,19 +8,66 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            return contents.ToArray();
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[,,] array3D = new string[length1, length2, length3];
+            int index = 0;
+            for (int i = 0; i < length1; i++)
+            {
+                for (int j = 0; j < length2; j++)
+                {
+                    for (int k = 0; k < length3; k++)
+                    {
+                        if (index < contents.Count)
+                        {
+                            array3D[i, j, k] = contents[index++];
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Number of elements in list must match array size");
+                        }
+                    }
+                }
+            }
+            return array3D;
         }
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[][] arrayJagged2D = new string[2][];
+            arrayJagged2D[0] = new string[countRow1];
+            arrayJagged2D[1] = new string[countRow2];
+            int reqNoOfElements = countRow1 + countRow2;
+
+            if (contents.Count < reqNoOfElements)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+
+            int index = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < countRow1; j++)
+                    {
+                        arrayJagged2D[0][j] = contents[index++];
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < countRow2; j++)
+                    {
+                        arrayJagged2D[1][j] = contents[index++];
+                    }
+                }
+            }
+            return arrayJagged2D;
         }
     }
 }
--- MoreDataTypes_Labs/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
+++ MoreDataTypes_Labs/Labs/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
@@ -11,24 +11,58 @@ namespace MoreTypes_Lib
         // returns a person's age at a given date, given their birth date.
         public static int AgeAt(DateTime birthDate, DateTime date)
         {
-            return -1;
+            if (birthDate > date)
+            {
+                throw new ArgumentException("Error - birthDate is in the future");
+            }
+
+            int ageInYears = date.Year - birthDate.Year;
+
+            if (date.Month < birthDate.Month)
+            {
+                ageInYears--;
+            }
+            return ageInYears;
         }
         // returns a date formatted in the manner specified by the unit test
         public static string FormatDate(DateTime date)
         {
-            return string.Empty;
+            return date.ToString("yy/dd/MMM");
         }
 
         // returns the name of the month corresponding to a given date
         public static string GetMonthString(DateTime date)
         {
-            return string.Empty;
+            return date.ToString("MMMM");
         }
 
         // see unit tests for requirements
         public static string Fortune(Suit suit)
         {
-            return string.Empty;
+            switch (suit)
+            {
+                case Suit.CLUBS:
+                    {
+                        return "And the seventh rule is if this is your first night at fight club, you have to fight.";
+                    }
+                case Suit.DIAMONDS:
+                    {
+                        return "Diamonds are a girls best friend";
+                    }
+                case Suit.HEARTS:
+                    {
+                        return "You've broken my heart";
+                    }
+                case Suit.SPADES:
+                    {
+                        return "Bucket and spade";
+                    }
+                default:
+                    {
+                        return "No unit test for this";
+                    }
+
+            }
         }
     }
 }
